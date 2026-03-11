24717de3cca494cdc36be2d5b2cbbd93570243c0
Carlos Sanchez <carloscaes03@gmail.com>
2026-03-11 17:50:02 -0500

    Se crea el proyecto clintec

---
+++ src/main/java/com/terreneitors/backendclintec/BackendClintecApplication.java
@@ -0,0 +1,13 @@
+package com.terreneitors.backendclintec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendClintecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendClintecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terreneitors/backendclintec/BackendClintecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terreneitors.backendclintec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClintecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
